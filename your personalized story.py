8d1b6efccc5a47af0fac512f7773a9b55166d09e
srisushant1234 <32979303+srisushant1234@users.noreply.github.com>
2018-03-29 18:01:49 +0530

    Add files via upload

---
+++ your personalized story.py
@@ -0,0 +1,22 @@
+print("Welcome to your own personalized story")
+print("What is your name?")
+name1=input()
+name1=name1.capitalize()
+print("What is your friend's name")
+name2=input()
+name2=name2.capitalize()
+ 
+print("Name your favourite wild animal/carnivore")
+animal1=input()
+
+print(name1 + ' and' + name2 +''' were friends. On a holiday they went into a forest.
+They were enjoying the beauty of nature. Suddenly they saw a'''+ animal1+'''coming at them.
+They were frightened.'''+ name2+'''who knew climbing trees ran up to a tree and climbed up quickly.
+He did not think of'''+ name1+'''.'''+ name1+''' did not know tree climbing.''' 
++ name1+'''thought for a second.He had heard animals do not prefer dead bodies.
+He fell to the ground and held his breath.
+The animal1 sniffed him and thought he was dead. So, he went away.''' 
++ name2+'''asked name1,What did the'''+ animal1+'''whisper into your ears?" .
+name1 replied, "The'''+ animal1+''' asked me to keep away from friends like you" and went on his way.
+
+        MORAL : A friend in need is a friend indeed.''')
